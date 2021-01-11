79f01a29678992516f7fdb841c2b56cc53979cb1
lubio-305 <lubiopolice@gmail.com>
2021-01-11 18:11:00 +0800

    products loist continue

--- products.py
+++ products.py
@@ -1,4 +1,15 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue #繼續
+		name, price = line.strip().split(',')
+		products.append([name,price])
+
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -7,7 +18,13 @@ while True:
 	products.append([name,price]) 
 print(products)
 
+# 印出所有商品紀錄
 for p in products:
 	print(p[0], '的價格是',p[1])
-	 
+
+# 許入檔案
+with open('products.csv', 'w', encoding='utf-8') as f: 
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
 
